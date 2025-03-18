c2cb6e43be2f19073de444b76a212aade22b95fa
Jay-S99 <93200102+Jay-S99@users.noreply.github.com>
2025-03-18 22:41:33 +0100

    GKlab

---
+++ Lab2cw2.py
@@ -0,0 +1,30 @@
+import pygame
+
+pygame.init()
+win = pygame.display.set_mode((600, 600))
+pygame.display.set_caption("Wariant 4")
+
+
+BIALY = (255, 255, 255)
+CZERWONY = (255, 0, 0)
+
+def rysuj_Z(okno):
+    # Górna belka Z
+    pygame.draw.rect(okno, CZERWONY, (150, 150, 300, 50))
+    # Dolna belka Z
+    pygame.draw.rect(okno, CZERWONY, (150, 400, 300, 50))
+    # Przekątna Z
+    pygame.draw.polygon(okno, CZERWONY, [(150, 400), (450, 150), (450, 200), (150, 450)])
+
+run = True
+while run:
+    win.fill(BIALY)  # Czyszczenie ekranu
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    
+    rysuj_Z(win)  # Rysowanie litery Z
+    pygame.display.update()
+    
+pygame.quit()
